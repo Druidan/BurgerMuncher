c338e83b61a41043abf3f37051e79b87290c674c
Druidan <44009123+Druidan@users.noreply.github.com>
2019-04-26 15:32:02 -0500

    burger controller now has a put route

--- controllers/burger_controller.js
+++ controllers/burger_controller.js
@@ -10,26 +10,48 @@ const burger = require('../models/burger_model.js');
 //-------------------------------------
 
 router.get('/', (req, res) => {
-    burger.selectAll( data => {
+
+    burger.selectAll(data => {
         const handlebarsObj = {
             burgers: data
         };
         res.render('index', handlebarsObj);
     });
+
 });
 
 //-------------------------------------
 
 router.post('/api/burgers', (req, res) => {
-    burger.createBurger(req.body.newBurger, function(result) {
-        res.json({ id: result.insertId });
+
+    burger.createBurger(req.body.newBurger, function (result) {
+        res.json({
+            id: result.insertId
+        });
     });
-});
-
-//-------------------------------------
-
-router.update('/api/burgers/:id', (req, res) => {
 
 });
 
 //-------------------------------------
+
+router.put('/api/burgers/:id', (req, res) => {
+
+    const condition = `id = ${req.params.id}`;
+
+    burger.updateBurger({
+        munched: req.body.munched
+    }, condition, function (result) {
+        if (result.changedRows == 0) {
+            // If no rows were changed, then the ID must not exist, so 404
+            return res.status(404).end();
+        } else {
+            res.status(200).end();
+        }
+    });
+    
+});
+
+//-------------------------------------
+
+// Export routes for server.js to use.
+module.exports = router;
