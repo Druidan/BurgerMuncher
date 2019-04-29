5f015ac800b3557436fbaaf7b0ec074767e83837
Druidan <44009123+Druidan@users.noreply.github.com>
2019-04-28 20:40:19 -0500

    added the bit to connect to JAWSDB

--- config/connection.js
+++ config/connection.js
@@ -2,13 +2,19 @@
 require(`dotenv`).config();
 const mysql = require(`mysql`);
 
-const connection = mysql.createConnection( {
-    host: "localhost",
-    port: 3306,
-    user: "root",
-    password: process.env.M_PASS,
-    database: "burgers_db"
-});
+const connection;
+
+if(process.env.JAWSDB_URL) {
+    connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+    connection = mysql.createConnection( {
+        host: "localhost",
+        port: 3306,
+        user: "root",
+        password: process.env.M_PASS,
+        database: "burgers_db"
+    });
+};
 
 connection.connect( err => {
     if (err) {
