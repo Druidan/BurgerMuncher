75df1a057fef0939a39915150298b34c82b03a15
Druidan <44009123+Druidan@users.noreply.github.com>
2019-04-28 21:12:48 -0500

    I've got a commented-out version of the animate function in the animate.js file that I'm working on. I hope if I write it better it'll work better on heroku (right now it drops frames)

--- public/js/animate.js
+++ public/js/animate.js
@@ -19,3 +19,31 @@ function animate(ele, srcArr, reps) {
         }, i*50);
     });
 }
+
+// function animate2(ele, srcArr, reps) {
+//     let i = 0
+//     let j = 0
+//     let sources = srcArr;
+//     let repetitions = reps;
+
+//     function changeImg() {
+
+//     }
+
+//     sources.forEach(src => {
+//         i++
+//         console.log(repetitions)
+//         setTimeout(() => {
+//             ele.forEach(e => {
+//                 e.src = src;
+//                 j++
+//                 if(j === sources.length && repetitions > 0){
+//                     repetitions--
+//                     animate(ele, sources, repetitions);
+//                 }
+//             })
+//         }, i*50);
+//     });
+
+
+// }
