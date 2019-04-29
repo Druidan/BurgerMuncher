3f3c381e084a0b9d106e60c1d2ab410da504e1a2
Druidan <44009123+Druidan@users.noreply.github.com>
2019-04-28 20:52:17 -0500

    fixing a silly const error

--- config/connection.js
+++ config/connection.js
@@ -2,7 +2,7 @@
 require(`dotenv`).config();
 const mysql = require(`mysql`);
 
-const connection;
+let connection;
 
 if(process.env.JAWSDB_URL) {
     connection = mysql.createConnection(process.env.JAWSDB_URL);
